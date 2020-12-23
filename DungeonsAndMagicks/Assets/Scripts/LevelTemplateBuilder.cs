889484148f27e7c3421119ac8537c204ece3bcaa
Jack <Jack>
2020-12-24 01:38:45 +1300

    LevelTemplateBuilder Tool
    Saves into a scriptableobject for easy save/load

---
+++ DungeonsAndMagicks/Assets/Scripts/LevelTemplateBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTemplateBuilder : MonoBehaviour
+{
+    [Header("Loaded Template")]
+    public LevelTemplate Template;
+
+    [Header("Create New Template")]
+    public string LevelName = "Level";
+    public string FilePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
