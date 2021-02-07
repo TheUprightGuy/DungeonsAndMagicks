7abde163cdd2cbe408c6d539a2c8b7d8c9fd38a0
BigGriz <waydbarton@hotmail.co.nz>
2021-02-07 16:39:30 +1300

    Fix

--- Assets/Slot.cs
+++ Assets/Slot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour//, IDropHandler
+public class Slot : MonoBehaviour, IDropHandler
 {
     public GameObject item
     {
